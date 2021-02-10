18323f52e5338edb7b164013f255d577f7c3d90f
Aaron Aizenman <aizenman.aaron@gmail.com>
2021-02-10 11:46:31 -0500

    First draft

--- classes.py
+++ classes.py
@@ -5,16 +5,41 @@ class Application(tk.Frame):
             super().__init__(master)
             self.master=master
             self.pack()
-            self.create_widgets()
 
-        def create_widgets(self):
-            self.hi_there = tk.Button(self)
-            self.hi_there["text"] = "Hello World\n(click me)"
-            self.hi_there["command"] = self.say_hi
-            self.hi_there.pack(side="top")
+            queryFields = "Start Date (yyyymmddhhMMSS)", "End Date (yyyymmddhhMMSS)", "Client Short ID", "Message Type (MT, MO, NL)", "System Type (PE, XMS, CA, NOBILL)", "Site", "Country (2 letter ISO Code)", "Operator ID", "MO State", "MT State", "MT Status", "Limit", "Timeout", "Format (txt, csv, tsv)"
 
-            self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-            self.quit.pack(side="bottom")
+            outputFields = "field1"
 
-        def say_hi(self):
-            print("hi there, everyone!")
+            buttonTypes = "GENERATE SCRIPT", "QUIT"
+
+            self.make_form(queryFields, buttonTypes, outputFields)
+
+        def make_form(self, queryFields, buttonTypes, outputFields):
+            self.create_fields(queryFields)
+            self.create_buttons(buttonTypes)
+
+        def create_fields(self, queryFields):
+            entries = []
+            for field in queryFields:
+                row = tk.Frame()
+                lab = tk.Label(row, width=30, text=field, anchor="w")
+                ent = tk.Entry(row)
+                row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+                lab.pack(side=tk.LEFT)
+                ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+                entries.append((field, ent))
+            return entries
+
+        def create_buttons(self, buttonTypes):
+            buttons = []
+            for button in buttonTypes:
+                row = tk.Frame()
+                if button == "QUIT":
+                    but = tk.Button(self, text=button, command=self.master.destroy, fg="red")
+                elif button == "GENERATE SCRIPT":
+                    but = tk.Button(self, text=button, command=self.gen_script(), fg="green")
+                row.pack(side=tk.TOP)
+                but.pack(side=tk.TOP)
+
+        def gen_script(self):
+            return True
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import tkinter as tk
 import classes
 
 root = tk.Tk()
+root.geometry("800x600")
 app = classes.Application(master=root)
 app.mainloop()
